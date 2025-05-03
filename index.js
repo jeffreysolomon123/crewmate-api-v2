a158999b5ed7fb01ab657bd32d87527bc02be3e4
jeffreysolomon123 <jeffreysolomon.official@gmail.com>
2025-05-03 16:45:25 +0530

    fix

--- index.js
+++ index.js
@@ -27,9 +27,25 @@ app.use(
 );
 
 app.use(express.urlencoded({ extended: true }));
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://crewmate-neon.vercel.app",
+];
+
 app.use(
-  cors({ credentials: true, origin: "https://crewmate-neon.vercel.app" })
-); // allow frontend to send cookies
+  cors({
+    credentials: true,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
+
 app.use(express.json());
 
 app.use(passport.initialize());
